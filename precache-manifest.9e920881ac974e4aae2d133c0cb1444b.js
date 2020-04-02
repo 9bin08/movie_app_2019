cd68ba2c3c7cc634df7d9a4c5080a917e1e75aae
김규빈 <9bin@gimgyubin-ui-MacBookPro.local>
2020-04-02 11:07:26 +0900

    Updates

---
+++ precache-manifest.9e920881ac974e4aae2d133c0cb1444b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a4cf77de3223cfca71747d9b172148",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "2dc1338b6e76f1f39558",
+    "url": "/movie_app_2019/static/css/main.3a6bde35.chunk.css"
+  },
+  {
+    "revision": "920ce3d01f32cc84dab8",
+    "url": "/movie_app_2019/static/js/2.7ea9e97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.7ea9e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc1338b6e76f1f39558",
+    "url": "/movie_app_2019/static/js/main.957e06c7.chunk.js"
+  },
+  {
+    "revision": "cabaf764d99e5ce71512",
+    "url": "/movie_app_2019/static/js/runtime-main.fe965e18.js"
+  }
+]);
